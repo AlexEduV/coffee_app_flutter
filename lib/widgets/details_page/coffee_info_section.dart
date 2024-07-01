57ad29d0ed413aa31f24dd4586c53e85344833eb
Alexander <m.mirvua@gmail.com>
2024-07-01 18:08:02 +0300

    added a blur effect to the coffee info in details;

--- lib/widgets/details_page/coffee_info_section.dart
+++ lib/widgets/details_page/coffee_info_section.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:coffee_app_flutter/helpers/text_helper.dart';
 import 'package:coffee_app_flutter/model/coffee.dart';
 import 'package:coffee_app_flutter/style/colors.dart';
@@ -18,117 +20,124 @@ class CoffeeInfoSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      padding: const EdgeInsets.symmetric(horizontal: 35.0, vertical: 25.0),
-      decoration: BoxDecoration(
-        color: ProjectColors.coffeeBackground.withOpacity(0.9),
-        borderRadius: BorderRadius.circular(32.0),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
 
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(32.0),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+        child: Container(
+          width: double.maxFinite,
+          padding: const EdgeInsets.symmetric(horizontal: 35.0, vertical: 25.0),
+          decoration: BoxDecoration(
+            color: ProjectColors.coffeeBackground.withOpacity(0.8),
+            borderRadius: BorderRadius.circular(32.0),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
 
-              Text(
-                coffee.name,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                ),
-              ),
-
-              Text(
-                coffee.subName,
-                style: const TextStyle(
-                  color: Colors.white54,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 16,
-                ),
-              ),
-
-              const Gap(16.0),
-
-              Row(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
 
-                  const Icon(
-                    FontAwesomeIcons.solidStar,
-                    color: ProjectColors.accentColor,
-                    size: 18,
+                  Text(
+                    coffee.name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+
+                  Text(
+                    coffee.subName,
+                    style: const TextStyle(
+                      color: Colors.white54,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    ),
                   ),
 
                   const Gap(16.0),
 
                   Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text(
-                        coffee.stars,
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold
-                        ),
+
+                      const Icon(
+                        FontAwesomeIcons.solidStar,
+                        color: ProjectColors.accentColor,
+                        size: 18,
                       ),
 
-                      const Gap(8.0),
+                      const Gap(16.0),
 
-                      Text(
-                        '(${formatter.format(coffee.reviews.toInt())})',
-                        style: const TextStyle(
-                          color: Colors.white38,
-                        ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            coffee.stars,
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+
+                          const Gap(8.0),
+
+                          Text(
+                            '(${formatter.format(coffee.reviews.toInt())})',
+                            style: const TextStyle(
+                              color: Colors.white38,
+                            ),
+                          ),
+
+                        ],
                       ),
-
                     ],
                   ),
+
+
+
+
                 ],
               ),
 
-
-
-
-            ],
-          ),
-
-          IntrinsicWidth(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-
-                Row(
+              IntrinsicWidth(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
 
-                    const DetailsItem(
-                      assetSource: 'assets/images/coffee-beans.png',
-                      text: 'Coffee',
+                    Row(
+                      children: [
+
+                        const DetailsItem(
+                          assetSource: 'assets/images/coffee-beans.png',
+                          text: 'Coffee',
+                        ),
+
+                        const Gap(16.0),
+
+                        DetailsItem(
+                          icon: FontAwesomeIcons.droplet,
+                          text: coffee.ingredient,
+                        ),
+
+
+                      ],
                     ),
 
                     const Gap(16.0),
 
-                    DetailsItem(
-                      icon: FontAwesomeIcons.droplet,
-                      text: coffee.ingredient,
-                    ),
-
+                    DetailsLargeItem(
+                      text: '${coffee.roastLevel} Roasted',
+                    )
 
                   ],
                 ),
-
-                const Gap(16.0),
-
-                DetailsLargeItem(
-                  text: '${coffee.roastLevel} Roasted',
-                )
-
-              ],
-            ),
-          )
-        ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
