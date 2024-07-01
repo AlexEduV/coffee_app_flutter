2e38447bd77c3ef237e1e87842d9a835ae993083
Alexander <m.mirvua@gmail.com>
2024-07-01 15:12:30 +0300

    refactoring in details page;

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,12 +1,10 @@
+import 'package:coffee_app_flutter/widgets/details_page/coffee_info_section.dart';
 import 'package:coffee_app_flutter/widgets/details_page/cup_size_selector_item.dart';
-import 'package:coffee_app_flutter/widgets/details_page/details_item.dart';
-import 'package:coffee_app_flutter/widgets/details_page/details_large_item.dart';
 import 'package:coffee_app_flutter/widgets/read_more_button.dart';
 import 'package:coffee_app_flutter/widgets/section_title.dart';
 import 'package:flutter/material.dart';
 
 import 'package:coffee_app_flutter/model/coffee.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
@@ -62,125 +60,7 @@ class _DetailsPageState extends State<DetailsPage> {
                     ),
                   ),
 
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      width: double.maxFinite,
-                      padding: const EdgeInsets.symmetric(horizontal: 35.0, vertical: 25.0),
-                      decoration: BoxDecoration(
-                        color: ProjectColors.coffeeBackground.withOpacity(0.9),
-                        borderRadius: BorderRadius.circular(32.0),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-
-                              Text(
-                                widget.coffee.name,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25,
-                                ),
-                              ),
-
-                              Text(
-                                widget.coffee.subName,
-                                style: const TextStyle(
-                                  color: Colors.white54,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                ),
-                              ),
-
-                              const Gap(16.0),
-
-                              Row(
-                                children: [
-
-                                  const Icon(
-                                    FontAwesomeIcons.solidStar,
-                                    color: ProjectColors.accentColor,
-                                    size: 18,
-                                  ),
-
-                                  const Gap(16.0),
-
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: [
-                                      Text(
-                                        widget.coffee.stars,
-                                        style: const TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold
-                                        ),
-                                      ),
-
-                                      const Gap(8.0),
-
-                                      Text(
-                                        '(${widget.coffee.reviews})',
-                                        style: const TextStyle(
-                                          color: Colors.white38,
-                                        ),
-                                      ),
-
-                                    ],
-                                  ),
-                                ],
-                              ),
-
-
-
-
-                            ],
-                          ),
-
-                          IntrinsicWidth(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-
-                                Row(
-                                  children: [
-
-                                    const DetailsItem(
-                                      icon: FontAwesomeIcons.mortarPestle,
-                                      text: 'Coffee',
-                                    ),
-
-                                    const Gap(16.0),
-
-                                    DetailsItem(
-                                      icon: FontAwesomeIcons.droplet,
-                                      text: widget.coffee.ingredient,
-                                    ),
-
-
-                                  ],
-                                ),
-
-                                const Gap(16.0),
-
-                                DetailsLargeItem(
-                                  text: '${widget.coffee.roastLevel} Roasted',
-                                )
-
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-
+                  //app bar
                   Positioned(
                     top: 20.0,
                     left: 25.0,
@@ -206,6 +86,15 @@ class _DetailsPageState extends State<DetailsPage> {
 
                       ],
                     ),
+                  ),
+
+                  Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: CoffeeInfoSection(
+                        coffee: widget.coffee,
+                      ),
                   ),
 
                 ],
---
+++ lib/widgets/details_page/coffee_info_section.dart
@@ -0,0 +1,134 @@
+import 'package:coffee_app_flutter/model/coffee.dart';
+import 'package:coffee_app_flutter/style/colors.dart';
+import 'package:coffee_app_flutter/widgets/details_page/details_item.dart';
+import 'package:coffee_app_flutter/widgets/details_page/details_large_item.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
+
+class CoffeeInfoSection extends StatelessWidget {
+
+  final Coffee coffee;
+
+  const CoffeeInfoSection({
+    required this.coffee,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      padding: const EdgeInsets.symmetric(horizontal: 35.0, vertical: 25.0),
+      decoration: BoxDecoration(
+        color: ProjectColors.coffeeBackground.withOpacity(0.9),
+        borderRadius: BorderRadius.circular(32.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Text(
+                coffee.name,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+
+              Text(
+                coffee.subName,
+                style: const TextStyle(
+                  color: Colors.white54,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+
+              const Gap(16.0),
+
+              Row(
+                children: [
+
+                  const Icon(
+                    FontAwesomeIcons.solidStar,
+                    color: ProjectColors.accentColor,
+                    size: 18,
+                  ),
+
+                  const Gap(16.0),
+
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        coffee.stars,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+
+                      const Gap(8.0),
+
+                      Text(
+                        '(${coffee.reviews})',
+                        style: const TextStyle(
+                          color: Colors.white38,
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ],
+              ),
+
+
+
+
+            ],
+          ),
+
+          IntrinsicWidth(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+
+                Row(
+                  children: [
+
+                    const DetailsItem(
+                      icon: FontAwesomeIcons.mortarPestle,
+                      text: 'Coffee',
+                    ),
+
+                    const Gap(16.0),
+
+                    DetailsItem(
+                      icon: FontAwesomeIcons.droplet,
+                      text: coffee.ingredient,
+                    ),
+
+
+                  ],
+                ),
+
+                const Gap(16.0),
+
+                DetailsLargeItem(
+                  text: '${coffee.roastLevel} Roasted',
+                )
+
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
